675acc330912c7f31a335f2e5d7f0cebea1ae7fb
antoniozzo <info@antoniorizzo.com>
2017-09-13 15:40:24 +0200

    updated stories

--- source/components/Button/stories.js
+++ source/components/Button/stories.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import { checkA11y } from 'storybook-addon-a11y'
 
 import Button from './'
 
-storiesOf('Button', module).add('Default', () => (
-    <Button onClick={action('Button clicked')}>
-        Just a button
-    </Button>
-))
+storiesOf('Button', module)
+    .addDecorator(checkA11y)
+    .add('Default', () => (
+        <Button onClick={action('Button clicked')}>
+            Just a button
+        </Button>
+    ))
---
+++ source/views/HomePage/stories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import { HomePage } from './'
+
+const stories = storiesOf('Pages/HomePage', module)
+
+stories.add('Default', () => (
+    <HomePage
+        title="Home"
+        content="This is a beautiful home page"
+        meta={{
+            title: 'Home page meta title',
+            description: 'Home page meta description',
+        }}
+    />
+))
